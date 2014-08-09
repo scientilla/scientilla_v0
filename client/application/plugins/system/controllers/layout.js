a02a8b5063b495f04e01436b8c10926fa44bb52b
Antonio De Luca <antonio.deluca@iit.it>
2014-08-09 19:47:15 +0200

    Updated license file path on all licensing headers.

--- client/application/plugins/system/controllers/layout.js
+++ client/application/plugins/system/controllers/layout.js
@@ -1,7 +1,7 @@
 /*
  * Scientilla v0.0.1 (http://www.scientilla.net)
  * Copyright 2014 Fondazione Istituto Italiano di Tecnologia (http://www.iit.it)
- * Licensed under MIT (https://github.com/scientilla/master/LICENSE)
+ * Licensed under MIT (https://github.com/scientilla/scientilla/blob/master/LICENSE)
  */
 
 angular.module("system").controller(
