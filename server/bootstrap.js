80363a11a0ad1b784421090118c99da2603ba6aa
Antonio De Luca <antonio.deluca@iit.it>
2014-08-13 22:33:29 +0200

    Added peer seeds static configuration.

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -24,6 +24,7 @@ var underscore = require("underscore");
 var application = express();
 
 var configuration = require("./configuration/default.js")();
+var seeds = require("./seeds.js")();
 
 var datasetsController = require("./application/plugins/dataset/controllers/default.js")();
 var peerDatasetsController = require("./application/plugins/dataset/controllers/peer-datasets.js")();
@@ -54,6 +55,7 @@ application.use("*", function(req, res, next) {
     req.jsonWebToken = jsonWebToken;    
     req.moment = moment;
     req.request = request;
+    req.seeds = seeds;
     req.underscore = underscore;    
     next();    
 });
