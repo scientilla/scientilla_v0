2bac94aa92554bd9e0e1917faa99fbe7f4c24053
chippino <chippino@antoniodeluca.scientilla.development.iit.it>
2014-08-29 08:27:33 +0200

    Added support to inline visualization of peers and repositories into the network panel.

--- client/application/plugins/network/controllers/browsing.js
+++ client/application/plugins/network/controllers/browsing.js
@@ -5,13 +5,24 @@
  */
 
 angular.module("network").controller(
-    "networkBrowsingController", ["$scope", "peersService", "activatedPeersService", "systemStatusService", "$window", "$location", function($scope, peersService, activatedPeersService, systemStatusService, $window, $location) {
+    "networkBrowsingController", ["$scope", "peersService", "repositoriesService", "activatedPeersService", "activatedRepositoriesService", "systemStatusService", "$window", "$location", function($scope, peersService, repositoriesService, activatedPeersService, activatedRepositoriesService, systemStatusService, $window, $location) {
         /* TO-DO: substitute with a directive. */
         /* code start */            
         $("#help-button").popover({
             trigger: "focus"
         });
-        /* code end */        
+        /* code end */  
+        
+        $scope.visualizationMode = $window.sessionStorage.visualizationMode ? $window.sessionStorage.visualizationMode : "L";
+        $scope.listingType = $window.sessionStorage.listingType ? $window.sessionStorage.listingType : "P";
+        
+        $scope.saveVisualizationMode = function() {
+            $window.sessionStorage.visualizationMode = $scope.visualizationMode;
+        }
+        
+        $scope.saveListingType = function() {
+            $window.sessionStorage.listingType = $scope.listingType;
+        }        
             
         $scope.generatePeerIdsSharingMap = function(aPeers) {
             var peerIdsSharingMap = {};
@@ -20,6 +31,14 @@ angular.module("network").controller(
             }
             return peerIdsSharingMap;
         }
+        
+        $scope.generateRepositoryIdsSharingMap = function(aRepositories) {
+            var repositoryIdsSharingMap = {};
+            for (lpKey in aRepositories) {
+                repositoryIdsSharingMap[aRepositories[lpKey]._id] = aRepositories[lpKey].sharing_status;
+            }
+            return repositoryIdsSharingMap;
+        }        
         
         $scope.changingActivatedPeerId = "";
         $scope.setPeerAsActivated = function(id) {
@@ -35,6 +54,21 @@ angular.module("network").controller(
                 systemStatusService.react(status);
             });
         }
+        
+        $scope.changingActivatedRepositoryId = "";
+        $scope.setRepositoryAsActivated = function(id) {
+            $scope.changingActivatedRepositoryId = id;
+            activatedRepositoriesService.setRepositoryAsActivated(
+                id,
+                $window.sessionStorage.token
+            ).success(function(data, status, headers, config) {
+                $scope.activatedRepositoryId = id;
+                $scope.changingActivatedRepositoryId = ""; 
+            }).error(function(data, status, headers, config) {
+                $scope.changingActivatedRepositoryId = "";
+                systemStatusService.react(status);
+            });
+        }        
         
         $scope.changingSharedPeerId = "";
         $scope.switchPeerSharingStatus = function(id) {
@@ -63,6 +97,34 @@ angular.module("network").controller(
                 });
             }
         }
+        
+        $scope.changingSharedRepositoryId = "";
+        $scope.switchRepositorySharingStatus = function(id) {
+            $scope.changingSharedRepositoryId = id;
+            if (!$scope.repositoryIdsSharingMap[id]) {
+                repositoriesService.setRepositoryAsShared(
+                    id,
+                    $window.sessionStorage.token
+                ).success(function(data, status, headers, config) {
+                    $scope.repositoryIdsSharingMap[id] = true;
+                    $scope.changingSharedRepositoryId = ""; 
+                }).error(function(data, status, headers, config) {
+                    $scope.changingSharedRepositoryId = "";
+                    systemStatusService.react(status);
+                });
+            } else {
+                repositoriesService.setRepositoryAsNotShared(
+                    id,
+                    $window.sessionStorage.token
+                ).success(function(data, status, headers, config) {
+                    $scope.repositoryIdsSharingMap[id] = false;
+                    $scope.changingSharedRepositoryId = ""; 
+                }).error(function(data, status, headers, config) {
+                    $scope.changingSharedRepositoryId = "";
+                    systemStatusService.react(status);
+                });
+            }
+        }        
         
         $scope.empty = false;
         $scope.ready = false;
@@ -98,6 +160,46 @@ angular.module("network").controller(
                 $scope.peerIdsSharingMap = $scope.generatePeerIdsSharingMap($scope.aPeers);
                 callback();
             },
+            function(callback) {
+                $scope.ready = true;
+                callback();
+            }
+        ]);    
+
+        $scope.empty = false;
+        $scope.ready = false;
+        $scope.error = false;
+        async.series([
+            function(callback) {
+                $scope.aRepositories = [];
+                repositoriesService.getRepositories($window.sessionStorage.token).success(function(data, status, headers, config) {
+                    $scope.aRepositories = data;
+                    if ($scope.aRepositories.length === 0) {
+                        $scope.empty = true;
+                    }
+                    callback();
+                }).error(function(data, status, headers, config) {
+                    $scope.error = true;
+                    systemStatusService.react(status, callback);
+                });
+            },
+            function(callback) {
+                $scope.oActivatedRepository = {};
+                activatedRepositoriesService.getActivatedRepository($window.sessionStorage.token).success(function(data, status, headers, config) {
+                    $scope.oActivatedRepository = data;
+                    callback();
+                }).error(function(data, status, headers, config) {
+                    systemStatusService.react(status, callback);
+                });
+            },
+            function(callback) {
+                $scope.activatedRepositoryId = $scope.oActivatedRepository.repository_id;
+                callback();
+            },
+            function(callback) {
+                $scope.repositoryIdsSharingMap = $scope.generateRepositoryIdsSharingMap($scope.aRepositories);
+                callback();
+            },            
             function(callback) {
                 $scope.ready = true;
                 callback();
