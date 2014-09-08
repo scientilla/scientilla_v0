36d6f4a4b542a78a90d6034f4dc0b99e104d9f2e
Federico Bozzini <federico.bozzini@iit.it>
2014-09-08 17:45:34 +0200

    Added multiple selection to clone the references of a repository. The cloning operation is now done in one step without a confirmation screen.

--- client/application/plugins/reference/controllers/repository-browsing.js
+++ client/application/plugins/reference/controllers/repository-browsing.js
@@ -5,7 +5,7 @@
  */
 
 angular.module("reference").controller(
-    "repositoryReferencesBrowsingController", ["$scope", "$routeParams", "repositoryReferencesService", "activatedRepositoriesService", "repositoriesService", "systemStatusService", "$window", "$location", function($scope, $routeParams, repositoryReferencesService, activatedRepositoriesService, repositoriesService, systemStatusService, $window, $location) {
+    "repositoryReferencesBrowsingController", ["$scope", "$routeParams", "repositoryReferencesService", "activatedRepositoriesService", "repositoriesService", "systemStatusService", "$window", "$location", "referencesService", function($scope, $routeParams, repositoryReferencesService, activatedRepositoriesService, repositoriesService, systemStatusService, $window, $location, referencesService) {
         $scope.repositoryId = $routeParams.repositoryId;            
         $scope.aReferences = [];      
         $scope.currentPage = 1;
@@ -13,6 +13,51 @@ angular.module("reference").controller(
         $scope.firstPage = 1;
         $scope.oRepository = null;
         $scope.hasPaginationData = false;
+        $scope.allSelected = false;
+        
+
+        
+        $scope.cloneReference = function(reference) {
+            referencesService.createReference({
+                title: reference.title,
+                authors: reference.authors,
+                organizations: reference.organizations,
+                tags: reference.tags,
+                year: reference.year,
+                doi: reference.doi,
+                journal_name: reference.journal_name,
+                journal_acronym: reference.journal_acronym,
+                journal_pissn: reference.journal_pissn,
+                journal_eissn: reference.journal_eissn,
+                journal_issnl: reference.journal_issnl,
+                journal_volume: reference.journal_volume,
+                journal_year: reference.journal_year,
+                conference_name: reference.conference_name,
+                conference_acronym: reference.conference_acronym,
+                conference_place: reference.conference_place,
+                conference_year: reference.conference_year,
+                book_title: reference.book_title,
+                book_isbn: reference.book_isbn,
+                book_pages: reference.book_pages,
+                book_editor: reference.book_editor,
+                book_year: reference.book_year,
+                abstract: reference.abstract,
+                month: reference.month,
+                print_status: reference.print_status,
+                note: reference.note
+            }, $window.sessionStorage.token).success(function(data, status, headers, config) {
+//                console.log('cloned');
+            }).error(function(data, status, headers, config) {
+                systemStatusService.react(status);
+            });
+        };        
+
+        $scope.cloneSelectedReferences = function(){
+            var selectedReferences = _.filter($scope.aReferences, {selected: true});
+            _.each(selectedReferences, function(reference) {
+               $scope.cloneReference(reference); 
+            });
+        };
         
         $scope.retrievePrevReferences = function() {
             $scope.retrieveReferences(-1);
@@ -72,6 +117,10 @@ angular.module("reference").controller(
                         }
                         repositoryReferencesService.aReferences = $scope.extract($scope.oRepository.extractors, data);                   
                         $scope.aReferences = repositoryReferencesService.aReferences;
+                        _.each($scope.aReferences, function(reference) {
+                            reference.selected = false;
+                        });
+                        $scope.allSelected = false;
                         if ($scope.aReferences.length === 0) {
                             $scope.empty = true;
                         }                    
@@ -114,6 +163,31 @@ angular.module("reference").controller(
         };
         
         $scope.init = function(){
+            $scope.$watch(
+                'aReferences', 
+                function(newReferences) {
+                    if (_.isEmpty(newReferences)) {
+                        return;
+                    }
+                    var allSelected =  _.every(newReferences, { selected: true });
+                    if (!allSelected) {
+                        $scope.allSelected = allSelected;
+                    }
+                },
+                true
+            );
+            $scope.$watch(
+                'allSelected', 
+                function(newAllSelected) {
+                    var allSelected =  _.every($scope.aReferences, { selected: true });
+                    if (newAllSelected || allSelected) {
+                        _.each($scope.aReferences, function(reference) {
+                            reference.selected = newAllSelected;
+                        });
+                    }
+                }
+            );
+            
             repositoriesService.getRepository(
                 $scope.repositoryId,
                 $window.sessionStorage.token
