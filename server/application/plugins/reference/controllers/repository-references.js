76d14f7b231c1d70340e2eb22fcf1af47f13c899
Federico Bozzini <federico.bozzini@iit.it>
2014-09-09 19:19:33 +0200

    Made some basic refactoring of the cleaning function for the references.

--- server/application/plugins/reference/controllers/repository-references.js
+++ server/application/plugins/reference/controllers/repository-references.js
@@ -8,36 +8,42 @@ var model = require("../models/repository-references.js")();
 
 module.exports = function () {
     var createReference = function(_, reference) {
+        var cleanItem = function(item) {
+            if (typeof item === "string") {
+                return item.trim();
+            }
+            return item;
+        };
         var cleanedReference = {};
         
-        !_.isUndefined(reference.title) ? cleanedReference.title = reference.title.trim() : cleanedReference.title = "";            
-        !_.isUndefined(reference.authors) ? cleanedReference.authors = reference.authors.trim() : cleanedReference.authors = "";
-        !_.isUndefined(reference.organizations) ? cleanedReference.organizations = reference.organizations.trim() : cleanedReference.organizations = "";
-        !_.isUndefined(reference.tags) ? cleanedReference.tags = reference.tags.trim() : cleanedReference.tags = "";            
-        !_.isUndefined(reference.year) ? cleanedReference.year = reference.year : cleanedReference.year = "";
-        !_.isUndefined(reference.doi) ? cleanedReference.doi = reference.doi.trim() : cleanedReference.doi = "";    
-        !_.isUndefined(reference.journal_name) ? cleanedReference.journal_name = reference.journal_name.trim() : cleanedReference.journal_name = "";
-        !_.isUndefined(reference.journal_acronym) ? cleanedReference.journal_acronym = reference.journal_acronym.trim() : cleanedReference.journal_acronym = "";
-        !_.isUndefined(reference.journal_pissn) ? cleanedReference.journal_pissn = reference.journal_pissn.trim() : cleanedReference.journal_pissn = "";
-        !_.isUndefined(reference.journal_eissn) ? cleanedReference.journal_eissn = reference.journal_eissn.trim() : cleanedReference.journal_eissn = "";
-        !_.isUndefined(reference.journal_issnl) ? cleanedReference.journal_issnl = reference.journal_issnl.trim() : cleanedReference.journal_issnl = "";
-        !_.isUndefined(reference.journal_volume) ? cleanedReference.journal_volume = reference.journal_volume.trim() : cleanedReference.journal_volume = "";
-        !_.isUndefined(reference.journal_year) ? cleanedReference.journal_year = reference.journal_year.trim() : cleanedReference.journal_year = "";
-        !_.isUndefined(reference.conference_name) ? cleanedReference.conference_name = reference.conference_name.trim() : cleanedReference.conference_name = "";
-        !_.isUndefined(reference.conference_acronym) ? cleanedReference.conference_acronym = reference.conference_acronym.trim() : cleanedReference.conference_acronym = "";
-        !_.isUndefined(reference.conference_place) ? cleanedReference.conference_place = reference.conference_place.trim() : cleanedReference.conference_place = "";
-        !_.isUndefined(reference.conference_year) ? cleanedReference.conference_year = reference.conference_year.trim() : cleanedReference.conference_year = "";
-        !_.isUndefined(reference.book_title) ? cleanedReference.book_title = reference.book_title.trim() : cleanedReference.book_title = "";
-        !_.isUndefined(reference.book_isbn) ? cleanedReference.book_isbn = reference.book_isbn.trim() : cleanedReference.book_isbn = "";
-        !_.isUndefined(reference.book_pages) ? cleanedReference.book_pages = reference.book_pages.trim() : cleanedReference.book_pages = "";
-        !_.isUndefined(reference.book_editor) ? cleanedReference.book_editor = reference.book_editor.trim() : cleanedReference.book_editor = "";
-        !_.isUndefined(reference.book_year) ? cleanedReference.book_year = reference.book_year.trim() : cleanedReference.book_year = "";
-        !_.isUndefined(reference.abstract) ? cleanedReference.abstract = reference.abstract.trim() : cleanedReference.abstract = "";
-        !_.isUndefined(reference.month) ? cleanedReference.month = reference.month.trim() : cleanedReference.month = "";
-        !_.isUndefined(reference.print_status) ? cleanedReference.print_status = reference.print_status.trim() : cleanedReference.print_status = "";
-        !_.isUndefined(reference.note) ? cleanedReference.note = reference.note.trim() : cleanedReference.note = "";       
-        !_.isUndefined(reference.approving_status) ? cleanedReference.approving_status = reference.approving_status : cleanedReference.approving_status = "";
-        !_.isUndefined(reference.sharing_status) ? cleanedReference.sharing_status = reference.sharing_status : cleanedReference.sharing_status = "";
+        cleanedReference.title = !_.isUndefined(reference.title) ? cleanItem(reference.title) : "";            
+        cleanedReference.authors = !_.isUndefined(reference.authors) ? cleanItem(reference.authors) : "";
+        cleanedReference.organizations = !_.isUndefined(reference.organizations) ? cleanItem(reference.organizations) : "";
+        cleanedReference.tags = !_.isUndefined(reference.tags) ? cleanItem(reference.tags) : "";            
+        cleanedReference.year = !_.isUndefined(reference.year) ? cleanItem(reference.year) : "";
+        cleanedReference.doi = !_.isUndefined(reference.doi) ? cleanItem(reference.doi) : "";    
+        cleanedReference.journal_name = !_.isUndefined(reference.journal_name) ? cleanItem(reference.journal_name) : "";
+        cleanedReference.journal_acronym = !_.isUndefined(reference.journal_acronym) ? cleanItem(reference.journal_acronym) : "";
+        cleanedReference.journal_pissn = !_.isUndefined(reference.journal_pissn) ? cleanItem(reference.journal_pissn) : "";
+        cleanedReference.journal_eissn = !_.isUndefined(reference.journal_eissn) ? cleanItem(reference.journal_eissn) : "";
+        cleanedReference.journal_issnl = !_.isUndefined(reference.journal_issnl) ? cleanItem(reference.journal_issnl) : "";
+        cleanedReference.journal_volume = !_.isUndefined(reference.journal_volume) ? cleanItem(reference.journal_volume) : "";
+        cleanedReference.journal_year = !_.isUndefined(reference.journal_year) ? cleanItem(reference.journal_year) : "";
+        cleanedReference.conference_name = !_.isUndefined(reference.conference_name) ? cleanItem(reference.conference_name) : "";
+        cleanedReference.conference_acronym = !_.isUndefined(reference.conference_acronym) ? cleanItem(reference.conference_acronym) : "";
+        cleanedReference.conference_place = !_.isUndefined(reference.conference_place) ? cleanItem(reference.conference_place) : "";
+        cleanedReference.conference_year = !_.isUndefined(reference.conference_year) ? cleanItem(reference.conference_year) : "";
+        cleanedReference.book_title = !_.isUndefined(reference.book_title) ? cleanItem(reference.book_title) : "";
+        cleanedReference.book_isbn = !_.isUndefined(reference.book_isbn) ? cleanItem(reference.book_isbn) : "";
+        cleanedReference.book_pages = !_.isUndefined(reference.book_pages) ? cleanItem(reference.book_pages) : "";
+        cleanedReference.book_editor = !_.isUndefined(reference.book_editor) ? cleanItem(reference.book_editor) : "";
+        cleanedReference.book_year = !_.isUndefined(reference.book_year) ? cleanItem(reference.book_year) : "";
+        cleanedReference.abstract = !_.isUndefined(reference.abstract) ? cleanItem(reference.abstract) : "";
+        cleanedReference.month = !_.isUndefined(reference.month) ? cleanItem(reference.month) : "";
+        cleanedReference.print_status = !_.isUndefined(reference.print_status) ? cleanItem(reference.print_status) : "";
+        cleanedReference.note = !_.isUndefined(reference.note) ? cleanItem(reference.note) : "";       
+        cleanedReference.approving_status = !_.isUndefined(reference.approving_status) ? cleanItem(reference.approving_status) : "";
+        cleanedReference.sharing_status = !_.isUndefined(reference.sharing_status) ? cleanItem(reference.sharing_status) : "";
             
         return cleanedReference;
     };
