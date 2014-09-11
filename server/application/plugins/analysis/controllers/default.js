01cf0e83fe6c12ede2376506ce9e676625d73f1a
Antonio De Luca <antonio.deluca@iit.it>
2014-09-11 19:15:28 +0200

    Added empty analysis server plugin.

---
+++ server/application/plugins/analysis/controllers/default.js
@@ -0,0 +1,13 @@
+/*
+ * Scientilla v0.0.1 (http://www.scientilla.net)
+ * Copyright 2014 Fondazione Istituto Italiano di Tecnologia (http://www.iit.it)
+ * Licensed under MIT (https://github.com/scientilla/scientilla/blob/master/LICENSE)
+ */
+
+var model = require("../models/default.js")();
+
+module.exports = function () {
+    return {
+        
+    };
+};
