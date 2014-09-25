79ea46587d8fb69105b565d9a6ecf57b5e3475db
Federico Bozzini <federico.bozzini@iit.it>
2014-09-25 11:24:44 +0200

    Added function the get the user from an id.

--- server/application/plugins/user/models/default.js
+++ server/application/plugins/user/models/default.js
@@ -4,8 +4,22 @@
  * Licensed under MIT (https://github.com/scientilla/scientilla/blob/master/LICENSE)
  */
 
+var _ = require("lodash");
+
 module.exports = function () {
     return {
-       
+       getUser: function(usersCollection, userId, callback) {
+            usersCollection.findOne({ _id: userId }, function(err, user) {
+                if (err) {
+                    callback(err, null);
+                    return;
+                }
+                if (_.isNull(user)) {
+                    callback(new Error('No user found'), null);
+                }
+                
+                callback(null, user);
+            });                
+        }
     };
 };
