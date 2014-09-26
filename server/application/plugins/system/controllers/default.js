932f1a249be7e19f6303ddb14ec610dc887f6e21
Federico Bozzini <federico.bozzini@iit.it>
2014-09-26 16:57:55 +0200

    Modified checkUserCoerence. Types are numbers and not string.

--- server/application/plugins/system/controllers/default.js
+++ server/application/plugins/system/controllers/default.js
@@ -6,6 +6,8 @@
 
 var model = require("../models/default.js")();
 
+var _ = require("lodash");
+
 module.exports = function () {
     return {
         checkUserCoherence: function(req, res) {
@@ -16,16 +18,24 @@ module.exports = function () {
                     return;
                 } 
                 if (
-                    req.underscore.isEmpty(user.type) ||
-                    (user.type === "1" && req.underscore.isEmpty(user.first_name)) ||
-                    (user.type === "1" && req.underscore.isEmpty(user.middle_name)) ||
-                    (user.type === "1" && req.underscore.isEmpty(user.last_name)) ||
-                    (user.type === "2" && req.underscore.isEmpty(user.business_name)) ||
-                    (user.type === "1" && req.underscore.isEmpty(user.birth_date)) ||
-                    (user.type === "1" && req.underscore.isEmpty(user.birth_city)) ||
-                    (user.type === "1" && req.underscore.isEmpty(user.birth_state)) ||
-                    (user.type === "1" && req.underscore.isEmpty(user.birth_country)) ||
-                    (user.type === "1" && req.underscore.isEmpty(user.sex))
+                    _.isNull(user.type) ||
+                    (user.type === 0 && _.isEmpty(user.first_name)) ||
+                    (user.type === 0 && _.isEmpty(user.middle_name)) ||
+                    (user.type === 0 && _.isEmpty(user.last_name)) ||
+                    (user.type === 0 && _.isEmpty(user.birth_date)) ||
+                    (user.type === 0 && _.isEmpty(user.birth_city)) ||
+                    (user.type === 0 && _.isEmpty(user.birth_state)) ||
+                    (user.type === 0 && _.isEmpty(user.birth_country)) ||
+                    (user.type === 0 && _.isEmpty(user.sex)) || 
+                    (user.type === 1 && _.isEmpty(user.first_name)) ||
+                    (user.type === 1 && _.isEmpty(user.middle_name)) ||
+                    (user.type === 1 && _.isEmpty(user.last_name)) ||
+                    (user.type === 1 && _.isEmpty(user.birth_date)) ||
+                    (user.type === 1 && _.isEmpty(user.birth_city)) ||
+                    (user.type === 1 && _.isEmpty(user.birth_state)) ||
+                    (user.type === 1 && _.isEmpty(user.birth_country)) ||
+                    (user.type === 1 && _.isEmpty(user.sex)) || 
+                    (user.type === 2 && _.isEmpty(user.business_name))
                 ) {
                     res.status(500).end();
                     console.log('Unvalid User Type or User Information');
