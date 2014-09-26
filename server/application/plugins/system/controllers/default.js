3b7f43234766e0c33b2ec08eeee7ae85f9e886d1
Federico Bozzini <federico.bozzini@iit.it>
2014-09-26 12:06:30 +0200

    Corrected some identation and formatting.

--- server/application/plugins/system/controllers/default.js
+++ server/application/plugins/system/controllers/default.js
@@ -12,22 +12,21 @@ module.exports = function () {
             req.usersCollection.findOne({ _id: req.user.id }, function(err, user) {
                 if (err || req.underscore.isNull(user)) {
                     res.status(401).end();
-                } else {
-                    if (
-                        req.underscore.isEmpty(user.type) ||
-                        (user.type === "R" && req.underscore.isEmpty(user.first_name)) ||
-                        (user.type === "R" && req.underscore.isEmpty(user.middle_name)) ||
-                        (user.type === "R" && req.underscore.isEmpty(user.last_name)) ||
-                        (user.type === "O" && req.underscore.isEmpty(user.business_name)) ||
-                        (user.type === "R" && req.underscore.isEmpty(user.birth_date)) ||
-                        (user.type === "R" && req.underscore.isEmpty(user.birth_city)) ||
-                        (user.type === "R" && req.underscore.isEmpty(user.birth_state)) ||
-                        (user.type === "R" && req.underscore.isEmpty(user.birth_country)) ||
-                        (user.type === "R" && req.underscore.isEmpty(user.sex))
-                    ) 
-                    {
-                        res.status(500).end();
-                    }
+                    return;
+                } 
+                if (
+                    req.underscore.isEmpty(user.type) ||
+                    (user.type === "R" && req.underscore.isEmpty(user.first_name)) ||
+                    (user.type === "R" && req.underscore.isEmpty(user.middle_name)) ||
+                    (user.type === "R" && req.underscore.isEmpty(user.last_name)) ||
+                    (user.type === "O" && req.underscore.isEmpty(user.business_name)) ||
+                    (user.type === "R" && req.underscore.isEmpty(user.birth_date)) ||
+                    (user.type === "R" && req.underscore.isEmpty(user.birth_city)) ||
+                    (user.type === "R" && req.underscore.isEmpty(user.birth_state)) ||
+                    (user.type === "R" && req.underscore.isEmpty(user.birth_country)) ||
+                    (user.type === "R" && req.underscore.isEmpty(user.sex))
+                ) {
+                    res.status(500).end();
                 }
             });
         } 
