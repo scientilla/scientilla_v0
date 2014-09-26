8812aa1fa327b38a2df293fc05e446a818ca6573
Antonio De Luca <antonio.deluca@iit.it>
2014-09-26 12:45:18 +0200

    Fixed a problem with the checkUserCoherence function and the the user types.

--- server/application/plugins/system/controllers/default.js
+++ server/application/plugins/system/controllers/default.js
@@ -12,21 +12,23 @@ module.exports = function () {
             req.usersCollection.findOne({ _id: req.user.id }, function(err, user) {
                 if (err || req.underscore.isNull(user)) {
                     res.status(401).end();
+                    console.log('Unauthorized user');
                     return;
                 } 
                 if (
                     req.underscore.isEmpty(user.type) ||
-                    (user.type === "R" && req.underscore.isEmpty(user.first_name)) ||
-                    (user.type === "R" && req.underscore.isEmpty(user.middle_name)) ||
-                    (user.type === "R" && req.underscore.isEmpty(user.last_name)) ||
-                    (user.type === "O" && req.underscore.isEmpty(user.business_name)) ||
-                    (user.type === "R" && req.underscore.isEmpty(user.birth_date)) ||
-                    (user.type === "R" && req.underscore.isEmpty(user.birth_city)) ||
-                    (user.type === "R" && req.underscore.isEmpty(user.birth_state)) ||
-                    (user.type === "R" && req.underscore.isEmpty(user.birth_country)) ||
-                    (user.type === "R" && req.underscore.isEmpty(user.sex))
+                    (user.type === "1" && req.underscore.isEmpty(user.first_name)) ||
+                    (user.type === "1" && req.underscore.isEmpty(user.middle_name)) ||
+                    (user.type === "1" && req.underscore.isEmpty(user.last_name)) ||
+                    (user.type === "2" && req.underscore.isEmpty(user.business_name)) ||
+                    (user.type === "1" && req.underscore.isEmpty(user.birth_date)) ||
+                    (user.type === "1" && req.underscore.isEmpty(user.birth_city)) ||
+                    (user.type === "1" && req.underscore.isEmpty(user.birth_state)) ||
+                    (user.type === "1" && req.underscore.isEmpty(user.birth_country)) ||
+                    (user.type === "1" && req.underscore.isEmpty(user.sex))
                 ) {
                     res.status(500).end();
+                    console.log('Unvalid User Type or User Information');
                 }
             });
         } 
