53356548c2f588391535bc1d4d2521dd7f76ec77
Federico Bozzini <federico.bozzini@iit.it>
2014-10-14 16:34:27 +0200

    Used method with source.type to create a new reference.

--- client/application/plugins/reference/controllers/addition.js
+++ client/application/plugins/reference/controllers/addition.js
@@ -82,40 +82,17 @@ angular.module("reference").controller(
         };
         
         $scope.createReference = function() {
-            referencesService.createReference({
-                title: $scope.oReference.title,
-                authors: $scope.oReference.authors,
-                organizations: $scope.oReference.organizations,
-                tags: $scope.oReference.tags,
-                year: $scope.oReference.year,
-                doi: $scope.oReference.doi,
-                journal_name: $scope.oReference.journal_name,
-                journal_acronym: $scope.oReference.journal_acronym,
-                journal_pissn: $scope.oReference.journal_pissn,
-                journal_eissn: $scope.oReference.journal_eissn,
-                journal_issnl: $scope.oReference.journal_issnl,
-                journal_volume: $scope.oReference.journal_volume,
-                journal_year: $scope.oReference.journal_year,
-                conference_name: $scope.oReference.conference_name,
-                conference_acronym: $scope.oReference.conference_acronym,
-                conference_place: $scope.oReference.conference_place,
-                conference_year: $scope.oReference.conference_year,
-                book_title: $scope.oReference.book_title,
-                book_isbn: $scope.oReference.book_isbn,
-                book_pages: $scope.oReference.book_pages,
-                book_editor: $scope.oReference.book_editor,
-                book_year: $scope.oReference.book_year,
-                abstract: $scope.oReference.abstract,
-                month: $scope.oReference.month,
-                print_status: $scope.oReference.print_status,
-                note: $scope.oReference.note,              
-                approving_status: false,
-                sharing_status: false
-            }, $window.sessionStorage.token).success(function(data, status, headers, config) {
-                $location.path("browse-references");
-            }).error(function(data, status, headers, config) {
-                systemStatusService.react(status);
-            });
+            referencesService.createReferenceAsync(
+                $scope.oReference, 
+                $window.sessionStorage.token,
+                function(result) {
+                    switch (result.status) {
+                        case 200:
+                            $location.path("browse-references");
+                        default:
+                            systemStatusService.react(result.status);
+                    }
+                });
         };        
         
         $scope.init = function() {
