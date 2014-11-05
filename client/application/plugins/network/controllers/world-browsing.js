c0af4924934384447c672224cedcdaa2e5a0de8c
Antonio De Luca <antonio.deluca@iit.it>
2014-11-05 13:49:09 +0100

    Added world network references retrieve function.

--- client/application/plugins/network/controllers/world-browsing.js
+++ client/application/plugins/network/controllers/world-browsing.js
@@ -5,7 +5,7 @@
  */
 
 angular.module("network").controller(
-    "worldNetworkBrowsingController", ["$scope", "peersService", "repositoriesService", "activatedPeersService", "activatedRepositoriesService", "peerReferencesService", "seedPeerReferencesService", "repositoryReferencesService", "systemStatusService", "$window", "$location", function($scope, peersService, repositoriesService, activatedPeersService, activatedRepositoriesService, peerReferencesService, seedPeerReferencesService, repositoryReferencesService, systemStatusService, $window, $location) {
+    "worldNetworkBrowsingController", ["$scope", "worldNetworkReferencesService", "systemStatusService", "$window", "$location", function($scope, worldNetworkReferencesService, systemStatusService, $window, $location) {
         $scope.keywords = "";
         $scope.aReferences = [];
         $scope.startPageNumber = 1;
@@ -13,48 +13,18 @@ angular.module("network").controller(
         $scope.numberOfItemsPerPage = 25;
         $scope.totalNumberOfItems = 10000;                               
         
-        $scope.retrievePeersReferences = function() {
-            $scope.empty = false;
-            $scope.ready = false;
-            $scope.error = false;
-            for (var seedPeerIndex = 0; seedPeerIndex < $scope.aSeedPeers.length; seedPeerIndex++) {
-                async.series([
-                    function(callback) {
-                        seedPeerReferencesService.getReferences(
-                            seedPeerIndex,
-                            $scope.keywords,
-                            $window.sessionStorage.token
-                        ).success(function(data, status, headers, config) {
-                            $scope.aReferences = data;
-                            if ($scope.aReferences.length === 0) {
-                                $scope.empty = true;
-                            }                    
-                            callback();
-                        }).error(function(data, status, headers, config) {
-                            $scope.error = true;
-                            systemStatusService.react(status, callback);
-                        });
-                    },
-                    function(callback) {
-                        $scope.ready = true;
-                        callback();
-                    }
-                ]);
-            }
-        };        
-        
-        $scope.retrieveRepositoriesReferences = function() {
+        $scope.retrieveReferences = function retrieveReferences() {
             $scope.empty = false;
             $scope.ready = false;
             $scope.error = false;
             async.series([
                 function(callback) {
-                    repositoryReferencesService.getReferences(
-                        $scope.repositoryId,
-                        $window.sessionStorage.token,
-                        $scope.keywords
+                    worldNetworkReferencesService.getReferences(
+                        $scope.keywords,
+                        $scope.currentPageNumber,
+                        $scope.numberOfItemsPerPage,
+                        $window.sessionStorage.token
                     ).success(function(data, status, headers, config) {
-                        repositoryReferencesService.aReferences = data;                   
                         $scope.aReferences = data;
                         if ($scope.aReferences.length === 0) {
                             $scope.empty = true;
@@ -70,7 +40,9 @@ angular.module("network").controller(
                     callback();
                 }
             ]);
-        };
+            
+            return retrieveReferences;
+        }();
         
         $scope.retrievePreviousItemsPage = function() {
             if ($scope.startPageNumber > 1) {
