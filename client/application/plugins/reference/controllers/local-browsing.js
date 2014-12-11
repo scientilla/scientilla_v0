e05b9631b6f56c4b93c42547713d4f107992a6bd
Federico Bozzini <federico.bozzini@iit.it>
2014-12-11 18:05:52 +0100

    References: added bulk deletion.

--- client/application/plugins/reference/controllers/local-browsing.js
+++ client/application/plugins/reference/controllers/local-browsing.js
@@ -5,7 +5,9 @@
  */
 
 angular.module("reference").controller(
-    "localReferencesBrowsingController", ["$scope", "referencesService", "systemStatusService", "$window", "$location", function($scope, referencesService, systemStatusService, $window, $location) {
+    "localReferencesBrowsingController", 
+    ["$scope", "referencesService", "systemStatusService", "$window", "$location", "notificationService",
+    function($scope, referencesService, systemStatusService, $window, $location, notificationService) {
         $scope.userType = $window.sessionStorage.userType;
         $scope.userRights = $window.sessionStorage.userRights;
         $scope.keywords = "";
@@ -20,19 +22,19 @@ angular.module("reference").controller(
         
         $scope.generateReferenceIdsApprovingMap = function(aReferences) {
             var referenceIdsApprovingMap = {};
-            for (lpKey in aReferences) {
+            for (var lpKey in aReferences) {
                 referenceIdsApprovingMap[aReferences[lpKey]._id] = aReferences[lpKey].approving_status;
             }
             return referenceIdsApprovingMap;
-        }
+        };
         
         $scope.generateReferenceIdsSharingMap = function(aReferences) {
             var referenceIdsSharingMap = {};
-            for (lpKey in aReferences) {
+            for (var lpKey in aReferences) {
                 referenceIdsSharingMap[aReferences[lpKey]._id] = aReferences[lpKey].sharing_status;
             }
             return referenceIdsSharingMap;
-        }
+        };
         
         $scope.switchReferenceApprovingStatus = function(id) {
             $scope.changingApprovedReferenceId = id;
@@ -59,7 +61,7 @@ angular.module("reference").controller(
                     systemStatusService.react(status);
                 });
             }
-        }        
+        };    
         
         $scope.switchReferenceSharingStatus = function(id) {
             $scope.changingSharedReferenceId = id;
@@ -86,7 +88,7 @@ angular.module("reference").controller(
                     systemStatusService.react(status);
                 });
             }
-        }
+        };
                
         $scope.retrieveReferences = function retrieveReferences() {
             $scope.empty = false;
@@ -155,5 +157,99 @@ angular.module("reference").controller(
             }
             $scope.retrieveReferences();
         };
+        
+        $scope.selectAll = function() {
+            var allSelected = _.every($scope.aReferences, 'selected');
+            _.each($scope.aReferences, function(r){r.selected = !allSelected;});
+        };
+        
+        $scope.deleteSelected = function() {
+            async.parallel(
+                function(cb) {
+                    
+                }
+            );
+            $scope.deleteReference = function() {
+                referencesService.deleteReference({              
+                    id: $routeParams.id
+                }, $window.sessionStorage.userToken).success(function(data, status, headers, config) {
+                    $location.path("browse-references");
+                }).error(function(data, status, headers, config) {
+                    systemStatusService.react(status);
+                });
+            };
+        };
+        
+               
+        
+        $scope.deleteSelectedReferences = function(){
+            var selectedReferences = _.filter($scope.aReferences, {selected: true});
+            
+            var deleteReferences = 
+                _.map(selectedReferences, function(reference) {
+                   return function(callback) {
+                       referencesService.deleteReferenceAsync(
+                            reference._id,
+                            $window.sessionStorage.userToken,
+                            function(result) {
+                               callback(null, result);
+                            }
+                       );
+                   };
+                });
+                
+            var notifyResults = function(err, allResults){
+                if (err) {
+                    notificationService.info('Some error happened');
+                    return;
+                }
+                var deletedReferences = 0;
+                var notDeletedReferences = 0;
+                var errors = 0;
+                allResults.forEach(function(result) {
+                    switch(result.status) {
+                        case 200: 
+                            _.remove($scope.aReferences, {_id: result.referenceId} );
+                            deletedReferences++;
+                            break;
+                        case 409:
+                            notDeletedReferences++;
+                            break;
+                        case 404:
+                        case 500:
+                            systemStatusService.react(result.status);
+                            errors++;
+                            break;
+                        default:
+                            systemStatusService.react(result.status);
+                            errors++;
+                    }
+                });
+                var msg = "";
+                if (allResults.length === 0) {
+                    msg = 'No References Selected';
+                } else {
+                    if (deletedReferences > 0) {
+                        if (deletedReferences === 1) {
+                            msg += deletedReferences + ' Reference Deleted.\n';
+                        } else {
+                            msg += deletedReferences + ' References Not Deleted.\n';
+                        }
+                    }
+                    if (notDeletedReferences > 0 ) {
+                        if (notDeletedReferences === 1) {
+                            msg += notDeletedReferences + ' Reference could not be deleted.\n';
+                        } else {
+                            msg += notDeletedReferences + ' References could not be deleted.\n';
+                        }
+                    }
+                    if (errors > 0 ) {
+                        msg += 'Some error happened.\n';
+                    }
+                }
+                notificationService.info(msg);
+            };
+            async.parallel(deleteReferences, notifyResults);
+        };  
     }]
 );
