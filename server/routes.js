92b694b6ea871c2058758e32ee63473aecc1892f
Antonio De Luca <antonio.deluca@iit.it>
2015-01-12 17:44:43 +0100

    Update files version information.

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,5 @@
 /*
- * Scientilla v0.0.1 (http://www.scientilla.net)
- * Copyright 2014 Fondazione Istituto Italiano di Tecnologia (http://www.iit.it)
+ * Scientilla v1.0 Beta (http://www.scientilla.net)
+ * Copyright 2014-2015 Fondazione Istituto Italiano di Tecnologia (http://www.iit.it)
  * Licensed under MIT (https://github.com/scientilla/scientilla/blob/master/LICENSE)
  */
